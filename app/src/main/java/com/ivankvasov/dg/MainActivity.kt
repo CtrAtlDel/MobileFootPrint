788c86f3ac909780ddf90236e5c17c479c35eb2a
CtrAtlDelete <kvasovski34@gmail.com>
2023-05-09 15:29:03 +0300

    add half of memory sensors

--- app/src/main/java/com/ivankvasov/dg/MainActivity.kt
+++ app/src/main/java/com/ivankvasov/dg/MainActivity.kt
@@ -1,20 +1,23 @@
 package com.ivankvasov.dg
 
+import android.app.ActivityManager
 import android.content.res.Configuration
 import android.os.Bundle
+import android.os.Handler
 import android.support.v7.app.AppCompatActivity
 import java.io.File
 import java.text.SimpleDateFormat
 import java.util.*
-import java.util.concurrent.Flow
 
 
 class MainActivity : AppCompatActivity() {
     private lateinit var file: File
+    private var handler: Handler? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
 
         file = File(this.filesDir, "time.txt")  // Create a new file to store the time data
 
@@ -30,13 +33,29 @@ class MainActivity : AppCompatActivity() {
                 val orientationString =
                     if (orientation == Configuration.ORIENTATION_PORTRAIT) "Portrait" else "Landscape"
 
+                val activityManager = getSystemService(ACTIVITY_SERVICE) as ActivityManager
+                val runningProcesses = activityManager.runningAppProcesses.size
+
+                val memoryInfo = ActivityManager.MemoryInfo()
+
+                // Get memory info
+                activityManager.getMemoryInfo(memoryInfo)
+
+                // Print memory info
+//                println("Total memory: ${memoryInfo.totalMem}")
+                val availMem = memoryInfo.availMem
+                val threshold = memoryInfo.threshold
+
                 file.appendText("Time: $currentTime\n") // Get time
                 file.appendText("Core numbers: $numCores\n") // Get Number of cores
                 file.appendText("Orientation: $orientationString\n") // Get orientation of device
+                file.appendText("Running process: $runningProcesses\n") // Get orientation of device
+                file.appendText("Available memory: $availMem\n") // Get orientation of device
+                file.appendText("Treshhold memory: $threshold\n") // Get orientation of device
+
 
             }
         }, 0, 60000)
     }
-
 
 }
