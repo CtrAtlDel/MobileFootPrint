a176fecf1ef8b2d3c8dffda88b03478211481a2f
CtrAtlDelete <kvasovski34@gmail.com>
2023-05-09 15:13:08 +0300

    orientation add

--- app/src/main/java/com/ivankvasov/dg/MainActivity.kt
+++ app/src/main/java/com/ivankvasov/dg/MainActivity.kt
@@ -1,10 +1,12 @@
 package com.ivankvasov.dg
 
+import android.content.res.Configuration
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import java.io.File
 import java.text.SimpleDateFormat
 import java.util.*
+import java.util.concurrent.Flow
 
 
 class MainActivity : AppCompatActivity() {
@@ -19,8 +21,19 @@ class MainActivity : AppCompatActivity() {
         Timer().schedule(object :
             TimerTask() {  // Start a timer to write the current time to the file every minute
             override fun run() {
-                val currentTime = SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(Date())
-                file.appendText("$currentTime\n")
+
+                val currentTime = SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(Date()) // Get time
+
+                val numCores = Runtime.getRuntime().availableProcessors() // Number of cores
+
+                val orientation = resources.configuration.orientation // Get orientation
+                val orientationString =
+                    if (orientation == Configuration.ORIENTATION_PORTRAIT) "Portrait" else "Landscape"
+
+                file.appendText("Time: $currentTime\n") // Get time
+                file.appendText("Core numbers: $numCores\n") // Get Number of cores
+                file.appendText("Orientation: $orientationString\n") // Get orientation of device
+
             }
         }, 0, 60000)
     }
